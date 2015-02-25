b962917deb4c78782af074aeff4b5ca7be17ef21
a.m.balashov <a.m.balashov@gmail.com>
2015-02-25 13:33:08 +0200

    Version 0.1.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject loopme/db "0.1.6"
+(defproject loopme/db "0.1.7-SNAPSHOT"
   :description "Loopme postgresql connection logic."
   :url "http://loopme.biz"
   :license {:name "MIT license"
