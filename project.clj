d8410cb96fb96becce8cf0f10e96affb5a6fff86
DimkaGorhover <DimkaGorhover@github.com>
2015-09-23 10:34:34 +0300

    Version 0.1.12-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject loopme/db "0.1.11"
+(defproject loopme/db "0.1.12-SNAPSHOT"
   :description "Loopme postgresql connection logic."
   :url "http://loopme.biz"
   :license {:name "MIT license"
